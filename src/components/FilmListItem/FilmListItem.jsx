7d5a7f671ea5309829f95d67b301e34ed6767d41
polrmn <polrmn@gmail.com>
2023-02-13 16:49:40 +0200

    FilmListItem prop to

--- src/components/FilmListItem/FilmListItem.jsx
+++ src/components/FilmListItem/FilmListItem.jsx
@@ -1,29 +1,27 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { Image, ListItem } from "./FilmListItem.styled";
 
 const FilmListItem = ({film, from}) => {
 
-  const location = useLocation();
-  
-    return (
-      <ListItem>
-        <Link
-          to={location.pathname === '/' ? `movies/${film.id}` : `${film.id}`}
-          state={{ back: from }}
-        >
-          <Image
-            src={
-              film.poster_path
-                ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
-                : `https://skydomepictures.com/wp-content/uploads/2018/08/movie-poster-coming-soon-2.png`
-            }
-            alt={film.title || film.original_title}
-          />
-          <p>{film.title || film.original_title}</p>
-        </Link>
-      </ListItem>
-    );
+  return (
+    <ListItem>
+      <Link
+        to={`/movies/${film.id}`}
+        state={{ back: from }}
+      >
+        <Image
+          src={
+            film.poster_path
+              ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
+              : `https://skydomepictures.com/wp-content/uploads/2018/08/movie-poster-coming-soon-2.png`
+          }
+          alt={film.title || film.original_title}
+        />
+        <p>{film.title || film.original_title}</p>
+      </Link>
+    </ListItem>
+  );
 }
 
 FilmListItem.propTypes = {
